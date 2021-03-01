783f5934a80b68cad10715186e9353338a794266
githubvary <1147284120@qq.com>
2021-03-01 12:17:55 +0800

    2021年3月1日 删除沙箱支付相关功能

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,27 +1,17 @@
 package com.example.demo;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.example.demo.mapper.OrderInfoMapper;
-import com.example.demo.service.UserService;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-    @Autowired
-    OrderInfoMapper orderInfoMapper;
 
-    @Autowired
-    UserService userService;
 
     @Test
     void contextLoads() {
-//        System.out.println(orderInfoMapper.pageAdminOrderVO(new Page<>(1,10),"",0).getRecords());
-//        System.out.println(orderInfoMapper.pageAdminOrderVO(new Page<>(1,10),"",0).getTotal());
 
-        System.out.println(userService.getById('3'));
     }
 
 }
